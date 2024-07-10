d79327ff7731d25f8cdb41f03cd14fb93941ff18
Nymbo <129332110+Nymbo@users.noreply.github.com>
2024-07-10 17:41:18 -0400

    adding alyxsissy to footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,22 +51,7 @@ export default function RootLayout({
 
 				<footer className="text-center text-sm dark:text-gray-400 text-gray-600 font-mono">
 					<p>
-						<A href="https://github.com/ai-ng">ai-ng</A> /{" "}
-						<A href="https://nickoates.com">nick oates</A>
-					</p>
-					<p>
-						Built with{" "}
-						<A href="https://sdk.vercel.ai">Vercel AI SDK</A> &{" "}
-						<A href="https://groq.com">Groq</A>
-					</p>
-					<p>
-						<A href="https://github.com/ai-ng/magic-spell">
-							source
-						</A>{" "}
-						/{" "}
-						<A href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fai-ng%2Fmagic-spell&env=GROQ_API_KEY&envDescription=Groq%20API%20key%20from%20https%3A%2F%2Fconsole.groq.com%2Fkeys&project-name=magic-spell&repository-name=magic-spell&demo-title=Magic%20Spell&demo-description=AI%20prompting%20built%20into%20your%20%3Ctextarea%3E&demo-url=https%3A%2F%2Fmagic-spell.vercel.app&demo-image=https%3A%2F%2Fmagic-spell.vercel.app%2Fopengraph-image.png">
-							deploy
-						</A>
+						<A href="https://www.alyxsissy.com/">Alyxsissy</A>
 					</p>
 				</footer>
 
